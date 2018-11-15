436f2013deb81a09fac03c3a383830a22b198d2c
hkareem2222 <huseinkareem@gmail.com>
2018-11-15 11:52:41 -0600

    Initial Commit

---
+++ ReziLogistics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReziLogistics
+//
+//  Created by Husein Kareem on 11/15/18.
+//  Copyright © 2018 Rezi Logistics. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ReziLogisticsTests/ReziLogisticsTests.swift
@@ -0,0 +1,34 @@
+//
+//  ReziLogisticsTests.swift
+//  ReziLogisticsTests
+//
+//  Created by Husein Kareem on 11/15/18.
+//  Copyright © 2018 Rezi Logistics. All rights reserved.
+//
+
+import XCTest
+@testable import ReziLogistics
+
+class ReziLogisticsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
